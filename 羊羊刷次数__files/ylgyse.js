84c726159a2d954f822fe813aea1a7ca043b2f66
matepm <matepet@163.com>
2022-09-17 00:26:06 +0800

    metepet
    9/17

---
+++ 羊羊刷次数__files/ylgyse.js
@@ -0,0 +1,12 @@
+
+
+// layer.confirm(
+//       "已经为您刷通关,请返回游戏查看<br>更多福利请点击下方加入福利群",
+//       {
+//         btn: ["加入福利群", "好的"]
+//       },
+//       function () {
+//         window.open("https://api.2xb.cn/GroupJump/", "__blank");
+//       },
+//       function () {}
+// );
