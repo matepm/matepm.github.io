b0adae143ee0070a5607ecf04d646773c34d3c70
matepm <matepet@163.com>
2022-09-22 17:38:39 +0800

    matepm
    922

---
+++ test/scripts/tags/index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+describe('Tags', () => {
+  require('./asset_img');
+  require('./asset_link');
+  require('./asset_path');
+  require('./blockquote');
+  require('./code');
+  require('./gist');
+  require('./iframe');
+  require('./img');
+  require('./include_code');
+  require('./link');
+  require('./post_link');
+  require('./post_path');
+  require('./pullquote');
+});
